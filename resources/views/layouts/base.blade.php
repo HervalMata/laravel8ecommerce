1f15785d1a7e8ceea67bf07268e9c117ae967db1
Herval Mata <hervalmataamparo@gmail.com>
2021-10-26 19:14:50 -0300

    auth - admin and user authenticate

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -43,8 +43,8 @@
                     </div>
                     <div class="topbar-menu right-menu">
                         <ul>
-                            <li class="menu-item" ><a title="Register or Login" href="login.html">Login</a></li>
-                            <li class="menu-item" ><a title="Register or Login" href="register.html">Register</a></li>
+                            <li class="menu-item" ><a title="Cadastre-se ou Login" href="{{route('login')}}">Login</a></li>
+                            <li class="menu-item" ><a title="Cadastre-se ou Login" href="{{route('register')}}">Cadastro</a></li>
                             <li class="menu-item lang-menu menu-item-has-children parent">
                                 <a title="English" href="#"><span class="img label-before"><img src="{{ asset('assets/images/lang-en.png') }}" alt="lang-en"></span>English<i class="fa fa-angle-down" aria-hidden="true"></i></a>
                                 <ul class="submenu lang" >
@@ -68,6 +68,44 @@
                                     </li>
                                 </ul>
                             </li>
+                            @if(Route::has('login'))
+                                @auth
+                                    @if(Auth::user()->utype === 'ADM')
+                                        <li class="menu-item menu-item-has-children parent" >
+                                            <a title="Minha Conta" href="#">Minha Conta ({{ Auth::user()->name }})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                            <ul class="submenu curency" >
+                                                <li class="menu-item" >
+                                                    <a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
+                                                </li>
+                                                <li class="menu-item">
+                                                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sair</a>
+                                                </li>
+                                                <form id="logout-form" method="POST" action="{{ route('logout') }}">
+                                                    @csrf
+                                                </form>
+                                            </ul>
+                                        </li>
+                                    @else
+                                        <li class="menu-item menu-item-has-children parent" >
+                                            <a title="Minha Conta" href="#">Minha Conta ({{ Auth::user()->name }})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                            <ul class="submenu curency" >
+                                                <li class="menu-item" >
+                                                    <a title="Dashboard" href="{{ route('user.dashboard') }}">Dashboard</a>
+                                                </li>
+                                                <li class="menu-item">
+                                                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sair</a>
+                                                </li>
+                                                <form id="logout-form" method="POST" action="{{ route('logout') }}">
+                                                    @csrf
+                                                </form>
+                                            </ul>
+                                        </li>
+                                    @endif
+                                @else
+                                    <li class="menu-item" ><a title="Cadastre-se ou Login" href="{{route('login')}}">Login</a></li>
+                                    <li class="menu-item" ><a title="Cadastre-se ou Login" href="{{route('register')}}">Cadastro</a></li>
+                                @endif
+                            @endif
                         </ul>
                     </div>
                 </div>
---
+++ resources/views/livewire/user/user-dashboard-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    User or Customer Dashboard
+</div>
